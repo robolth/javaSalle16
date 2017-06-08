7d091cc59e1b802289f69258109d4d80705259a7
MathieuJuttet <mathieujuttet@gmail.com>
2017-06-08 09:55:21 +0200

    Fenetre de choix du fichier

--- javaSalle16/ColoriageEnSalle16.java
+++ javaSalle16/ColoriageEnSalle16.java
@@ -2,6 +2,8 @@ package javaSalle16;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
+import javax.swing.JFileChooser;
 
 // Cette classe initialise une carte (modèle), une vue et un controleur
 public class ColoriageEnSalle16 {
@@ -9,22 +11,56 @@ public class ColoriageEnSalle16 {
 	private Carte carte;
 	private Vue vue;
 	
+
+	// Choix du fichier dans une fenêtre
+	 
+			public class SelectionFichier {
+			    
+			        // Exemple numéro 1
+			        // Boîte de sélection de fichier à partir du répertoire
+			        // "home" de l'utilisateur
+			        {
+			            // création de la boîte de dialogue
+			            JFileChooser dialogue = new JFileChooser();
+			             
+			            // affichage
+			            dialogue.showOpenDialog(null);
+			             
+			            // récupération du fichier sélectionné
+			            System.out.println("Fichier choisi : " + dialogue.getSelectedFile());
+			        }
+			         
+			         
+
+			 
+			    }
+				
+	
+	
+	
+	
+	
 	
 	// Le constructeur prendra en argument l'adresse du fichier à colorer
 	public ColoriageEnSalle16() throws IOException{
 
 		Parser parser = new Parser();
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez saisir l'adresse de la carte à colorier : ");
-		String adresse = sc.nextLine();
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Veuillez saisir l'adresse de la carte à colorier : ");
+		
+		
+		JFileChooser dialogue = new JFileChooser();
+		dialogue.showOpenDialog(null);
+		System.out.println(dialogue.getSelectedFile());
+		String adresse = dialogue.getSelectedFile().getAbsolutePath();
 		
 		if (parser.lecture(adresse))
 		{
 			this.carte = parser.getCarte();
 			System.out.println("ColoriageEnSalle16 : génération de la carte terminée");
 
-			parser.ecriture(adresse + "_colorie.txt");
+//			parser.ecriture(adresse + "_colorie.txt");
 		
 		}
 		else 
